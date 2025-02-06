5fb475544c32aa21eda42eb52930ec95baf43fa7
Barkat28Singh <Barkat1213singh@gmail.com>
2025-02-06 16:38:09 -0500

    Favicon + Resume download features added

--- app/page.jsx
+++ app/page.jsx
@@ -23,14 +23,20 @@ const Home = () => {
             </p>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2 text-white hover:text-black"
+              {/* Adding a Downlaod Link */}
+              <a
+                href="assets/Resume/Barkat-Resume.pdf"
+                download="Barkat-Resume.pdf"
               >
-                <span>Download Resumé</span>
-                <FiDownload className="text-xl"></FiDownload>
-              </Button>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2 text-white hover:text-black"
+                >
+                  <span>Download Resumé</span>
+                  <FiDownload className="text-xl"></FiDownload>
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Social></Social>
               </div>
