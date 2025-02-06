6b2cc307d32c3f5400ccd39884fe7cd6a20188d5
Barkat28Singh <Barkat1213singh@gmail.com>
2025-02-06 15:27:09 -0500

    config editor error

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 import { JetBrains_Mono } from "next/font/google";
 
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   darkMode: ["class"],
   content: [
