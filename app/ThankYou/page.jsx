0d979c60aa506431483c3a88d66e081b7a7b84c9
Barkat28Singh <Barkat1213singh@gmail.com>
2025-03-05 21:00:36 -0500

    Adding contact form connected to email

--- app/ThankYou/page.jsx
+++ app/ThankYou/page.jsx
@@ -1,33 +1,33 @@
 "use client";
+
 import { motion } from "framer-motion";
+
 const ThankYou = () => {
   return (
-    <motion.section
+    <motion.main
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        transition: { delay: 0.6, duration: 0.4, ease: "easeIn" },
       }}
-      className="py-6"
+      className="flex flex-col min-h-screen"
     >
-      <div className="container mx-auto">
-        <div className="flex flex-col items-center justify-cen xl:pt-8 xl:pb-24">
-          {/* Text */}
-          <div className="text-center xl:text-left order-2 xl:order-none">
-            <h2 className="h2 mb-6">
-              Thank you For Contacing Me!<br></br>
+      <section className="flex-grow flex items-center justify-center py-12">
+        <div className="container mx-auto">
+          <div className="flex flex-col items-center justify-center text-center">
+            {/* Text */}
+            <h2 className="xl:text-6xl font-bold text-white text-2xl md:text-4xl mb-6">
+              Thank you for contacting me!
+              <br />
               <span className="text-accent">
-                I Will Get Back To You Shortly!
+                I will get back to you shortly!
               </span>
             </h2>
-            <span className="flex text-xl text-center justify-center ">
-              {" "}
-              Have A Great Day!
-            </span>
+            <p className="text-xl text-white/60">Have a great day!</p>
           </div>
         </div>
-      </div>
-    </motion.section>
+      </section>
+    </motion.main>
   );
 };
 
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,39 +3,54 @@
 import { Button } from "/components/ui/button";
 import { Textarea } from "/components/ui/textarea";
 import { Input } from "/components/ui/input";
-import Link from "next/link";
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "/components/ui/select";
-
+import { useRouter } from "next/navigation";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
 import { motion } from "framer-motion";
+import { useState } from "react";
+
 const info = [
+  { icon: <FaPhoneAlt />, title: "Phone", description: "+1(647)-675-4385" },
   {
-    icon: <FaPhoneAlt></FaPhoneAlt>,
-    title: "Phone",
-    description: "+1(647)-675-4385",
-  },
-  {
-    icon: <FaEnvelope></FaEnvelope>,
+    icon: <FaEnvelope />,
     title: "Email",
     description: "barkat01@alumni.utoronto.ca",
   },
-  {
-    icon: <FaMapMarkedAlt></FaMapMarkedAlt>,
-    title: "Address",
-    description: "Toronto, ON",
-  },
+  { icon: <FaMapMarkedAlt />, title: "Address", description: "Toronto, ON" },
 ];
 
 const Contact = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    service: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formspreeEndpoint = "https://formspree.io/f/mnnpangg";
+
+    const response = await fetch(formspreeEndpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      router.push("/ThankYou"); // Redirect to thank you page on success
+    } else {
+      alert("There was an error submitting the form. Please try again.");
+    }
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -47,78 +62,96 @@ const Contact = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          {/* contact form */}
-          <div className="xl:h-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <div className="flex flex-row gap-4 bg-[#27272c] xl:text-[30px] justify-center items-center  ">
-                <h3 className=" flex  text-accent group-hover:text-white justify-center text-center">
-                  Let's Work Together
-                </h3>
-                <h3 className=" flex  text-white justify-center text-center">
-                  &
-                </h3>
-                <h3 className=" flex text-accent group-hover:text-white  justify-center text-center">
-                  Create Innovative Solutions
-                </h3>
-              </div>
-              {/* input area  */}
+          {/* contact form (set to 50% width on desktop) */}
+          <div className="xl:h-[54%] order-2 xl:order-none xl:w-1/2">
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+            >
+              <Input
+                type="text"
+                name="firstname"
+                placeholder="Firstname"
+                required
+                value={formData.firstname}
+                onChange={handleChange}
+              />
+              <Input
+                type="text"
+                name="lastname"
+                placeholder="Lastname"
+                required
+                value={formData.lastname}
+                onChange={handleChange}
+              />
+              <Input
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                required
+                value={formData.email}
+                onChange={handleChange}
+              />
+              <Input
+                type="tel"
+                name="phone"
+                placeholder="Phone Number"
+                required
+                value={formData.phone}
+                onChange={handleChange}
+              />
 
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Firstname"></Input>
-                <Input type="lastname" placeholder="Lastname"></Input>
-                <Input type="email" placeholder="Email Address"></Input>
-                <Input type="phone" placeholder="Phone Number"></Input>
+              {/* Service Selection - Styled */}
+              <div className="w-full">
+                <select
+                  name="service"
+                  className="w-full p-3 bg-[#1f1f24] text-white border border-white/20 rounded-md"
+                  required
+                  value={formData.service}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled>
+                    Select a service
+                  </option>
+                  <option value="Web-Dev-Design">
+                    Web Development & Design
+                  </option>
+                  <option value="UX/UI-Design">UI/UX Design</option>
+                  <option value="Graphic-Design">Graphic Design</option>
+                  <option value="Product-Design">Product Design</option>
+                  <option value="Motion-Design">Motion Design</option>
+                  <option value="Game-Design">Game Design</option>
+                </select>
               </div>
-              {/* multi select option */}
 
-              <Select>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select a service"></SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup className=" bg-[#27272c]">
-                    <SelectLabel>Select a service</SelectLabel>
-                    <SelectItem value="wd">Web Development & Design</SelectItem>
-                    <SelectItem value="ix">UI/UX Design</SelectItem>
-                    <SelectItem value="grd">Graphic Design</SelectItem>
-                    <SelectItem value="pd">Product Design</SelectItem>
-                    <SelectItem value="md">Motion Design</SelectItem>
-                    <SelectItem value="gd">Game Design</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              {/* Text area */}
               <Textarea
+                name="message"
+                placeholder="Anything else you want to mention?"
                 className="h-[200px]"
-                placeholder="Anything else you want to mention? Type here..."
-              ></Textarea>
-              {/* Submit Btn */}
-              <Link
-                href="/ThankYou"
-                className="flex items-center justify-center"
-              >
-                <Button size="lg" className="p-5 flex ">
-                  Submit Form
-                </Button>
-              </Link>
+                value={formData.message}
+                onChange={handleChange}
+              />
+
+              <Button type="submit" size="lg" className="p-5 flex">
+                Submit Form
+              </Button>
             </form>
           </div>
-          {/* information  section */}
-          <div className="flex flex-1 items-center xl:justify-end order-1 xl:order-nonne mb-8 xl:mb-0">
+
+          {/* Contact Information Section */}
+          <div className="flex flex-1 items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
             <ul className="flex flex-col gap-10">
-              {info.map((item, index) => {
-                return (
-                  <li key={index} className="flex items-center gap-6">
-                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#2727c] text-accent rounded-md flex items-center justify-center">
-                      <div className="text-[28px]">{item.icon}</div>
-                    </div>
-                    <div className="flex-1">
-                      <p className="text-white/60">{item.title}</p>
-                      <h3 className="text-xl">{item.description}</h3>
-                    </div>
-                  </li>
-                );
-              })}
+              {info.map((item, index) => (
+                <li key={index} className="flex items-center gap-6">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#2727c] text-accent rounded-md flex items-center justify-center">
+                    <div className="text-[28px]">{item.icon}</div>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+                    <h3 className="text-xl">{item.description}</h3>
+                  </div>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
